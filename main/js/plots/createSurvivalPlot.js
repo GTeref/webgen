6c419eb6bcf3406ecabe37e7aeab3288854811bb
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2025-05-18 21:04:15 -0400

    Adding more functionality to survival plots

--- main/js/plots/createSurvivalPlot.js
+++ main/js/plots/createSurvivalPlot.js
@@ -125,7 +125,7 @@ const createSurvivalPlotByCohort = function(survivalCurvesByCohort) {
   d3.select("#survivalPlot").html("");
   
   // Set up dimensions and margins
-  const margin = {top: 50, right: 150, bottom: 50, left: 50};
+  const margin = {top: 50, right: 200, bottom: 50, left: 100};
   const width = 800 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
   
